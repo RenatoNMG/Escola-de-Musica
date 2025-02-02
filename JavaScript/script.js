08435a194e6b1eef604fc84dc6ee0fa314490866
renatonm <renatonm9@gmail.com>
2025-02-02 20:50:42 -0300

    Site de Musica Lapa tito

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,2 +1,34 @@
 
+const contato = document.getElementById("contato");
+const drop = document.getElementById("drop");
+
+
+
+contato.addEventListener("click", ()=>{
+    drop.classList.toggle("esconder");
+
+})
+
 gsap.to(".elemento", { scale: 10, duration: 2 });
+gsap.to(".nota", { x: 1000, duration: 3, repeat: 2 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: 1,   
+        spaceBetween: 10,  
+        loop: true,  
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+         },
+    });
+});
